041649fa676f0aeca6dc1aa1d9eb27667bddfc48
rs147 <rs147@BEANBOI>
2025-11-04 16:11:06 +0000

    Add project files.

---
+++ Software Engineering Group/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Software_Engineering_Group.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
