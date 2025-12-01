4121d5c91bf1b8eadaa4ae094a1a69b0d7f386d1
rs147 <rs147@BEANBOI>
2025-12-01 14:46:46 +0000

    More Tests

--- SensoreTests/ReportsControllerTest.cs
+++ SensoreTests/ReportsControllerTest.cs
@@ -1,12 +1,13 @@
 ﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.Extensions.Options;
+using SensoreApp.Controllers;
+using SensoreApp.Models;
 using Software_Engineering_Group.Data;
 using Software_Engineering_Group.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.InMemory;
-using SensoreApp.Models;
-using SensoreApp.Controllers;
-using Microsoft.AspNetCore.Mvc;
 
 namespace SensoreTests
 {
@@ -21,7 +22,8 @@ namespace SensoreTests
 
             context.Report.AddRange(
                 new Report { ID = 1, userID = 101, reportInfo = "Report 1", staffID = 201, staffResponse = "Response 1" },
-                new Report { ID = 2, userID = 102, reportInfo = "Report 2", staffID = 202, staffResponse = "Response 2" }
+                new Report { ID = 2, userID = 102, reportInfo = "Report 2", staffID = 202, staffResponse = "Response 2" },
+                new Report { ID = 3, userID = 103, reportInfo = "Report 3", staffID = null, staffResponse = null }
             );
             context.SaveChanges();
             return context;
@@ -35,7 +37,32 @@ namespace SensoreTests
             var controller = new ReportsController(context);
             var result = await controller.Index() as ViewResult;
             var model = Assert.IsAssignableFrom<IEnumerable<Report>>(result.Model);
-            Assert.Equal(2, ((List<Report>)model).Count);
+
+            Assert.Equal(3, ((List<Report>)model).Count);
         }
+
+        [Fact]
+        public async Task StaffCanBeNullable()
+        {
+            var options = new DbContextOptionsBuilder<Software_Engineering_GroupContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabaseNullable")
+                .Options;
+            var context = new Software_Engineering_GroupContext(options);
+
+            context.Report.AddRange(
+                new Report { ID = 1, userID = 101, reportInfo = "Report 1", staffID = 201, staffResponse = "Response 1" },
+                new Report { ID = 2, userID = 102, reportInfo = "Report 2", staffID = 202, staffResponse = "Response 2" },
+                new Report { ID = 3, userID = 103, reportInfo = "Report 3", staffID = null, staffResponse = null }
+            );
+            context.SaveChanges();
+
+            var controller = new ReportsController(context);
+            var result = await controller.Index() as ViewResult;
+            var model = Assert.IsAssignableFrom<IEnumerable<Report>>(result.Model);
+            var report = model.First(r => r.ID == 3);
+            Assert.Null(report.staffID);
+            Assert.Null(report.staffResponse);
+        }
+
     }
 }
--- Software Engineering Group/Controllers/ReportsController.cs
+++ Software Engineering Group/Controllers/ReportsController.cs
@@ -51,8 +51,6 @@ namespace SensoreApp.Controllers
         }
 
         // POST: Reports/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ID,userID,reportInfo,staffID,staffResponse")] Report report)
